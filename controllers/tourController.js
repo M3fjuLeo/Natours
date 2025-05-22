ec1016aae9e69ef3e105679041101fef88588f19
M3fjuLeo <mateuszropek@onet.pl>
2025-05-22 20:12:15 +0200

    Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,21 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // Build query
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const query = await Tour.find(queryObj);
+
+    // const query = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // Execiute query
+    const tours = query;
 
     res.status(200).json({
       status: 'success',
