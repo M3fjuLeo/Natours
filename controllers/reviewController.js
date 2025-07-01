10164d4134bfc4d85bcabc26963d3e2b68660796
M3fjuLeo <mateuszropek@onet.pl>
2025-07-01 13:15:10 +0200

    Bulding Handler Factory Functions: Delete

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,8 +1,12 @@
 const catchAsync = require('../utils/catchAsync');
 const Review = require('./../models/reviewModel');
+const factory = require('./handlerFactory');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+
+  const reviews = await Review.find(filter);
 
   res.status(200).json({
     status: 'success',
@@ -23,3 +27,5 @@ exports.createReview = catchAsync(async (req, res, next) => {
     data: { review: newReview },
   });
 });
+
+exports.deleteReview = factory.deleteOne(Review);
