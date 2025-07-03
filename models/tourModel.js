f2c9204a2573a5bd5b1e8e41cc52a29a4d5a663d
M3fjuLeo <mateuszropek@onet.pl>
2025-07-03 12:29:33 +0200

    Calculating Average Rating on Tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -126,13 +126,6 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
-// tourSchema.pre('save', async function (next) {
-//   const guidesPromises = this.guides.map(async (id) => await User.findById(id));
-//   this.guides = await Promise.all(guidesPromises);
-
-//   next();
-// });
-
 // QUERY MIDDLEWARE
 tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
