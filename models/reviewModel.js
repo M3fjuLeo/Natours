8970437ab560a92651e54cba78b173530a586fad
M3fjuLeo <mateuszropek@onet.pl>
2025-07-05 12:04:19 +0200

    Geospatial Queries: Finding Tours Within Radius

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -21,6 +21,9 @@ const reviewSchema = new mongoose.Schema(
   { toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
 
+// Only one review per user
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'user',
